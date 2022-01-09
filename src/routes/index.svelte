15022b2cb52c31eec1437acd437d26f4757258cb
BernardKum <newbieforlife01@gmail.com>
2022-01-09 03:22:01 +0000

    commit 3

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,9 +7,10 @@
    	 if (error) alert(error.message); // alert if error
     }
 
+
 </script>
 
 <!-- Sign Out -->
 <section class="container px-4 py-3 text-center">
     <button class="btn btn-secondary" on:click={logout}>Logout</button>
-</section>	
+</section>
